8298b3b4eba0ff4eac006726c6464bd149cf19dd
Nikola Paunovic <nikola@applycolo.rs>
2024-11-03 23:58:30 +0100

    @vzsg twist on safety

--- Sources/App/Middleware/Compression.swift
+++ Sources/App/Middleware/Compression.swift
@@ -5,16 +5,20 @@ struct GzipMiddleware: AsyncMiddleware {
     func respond(to req: Request, chainingTo next: AsyncResponder) async throws -> Response {
         let response = try await next.respond(to: req)
 
-        // Only compress if the response should be compressed and is not empty
-        if let body = response.body.data, !body.isEmpty {
-            // Compress the response body using Gzip
-            let compressedData = try body.gzipped()
-            response.body = .init(data: compressedData)
-
-            // Set the appropriate headers
-            response.headers.replaceOrAdd(name: .contentEncoding, value: "gzip")
-            response.headers.replaceOrAdd(name: .contentLength, value: "\(compressedData.count)")
+        guard req.headers[.acceptEncoding].contains(where: { $0.contains("gzip") }) else {
+            return response
         }
+
+        guard let body = response.body.data, !body.isEmpty else {
+            return response
+        }
+
+        // Compress the response body using Gzip
+        let compressedData = try body.gzipped()
+        response.body = .init(data: compressedData)
+
+        // Set the appropriate headers
+        response.headers.replaceOrAdd(name: .contentEncoding, value: "gzip")
 
         return response
     }
